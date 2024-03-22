7647a5a1538dbc8c9a77e4908ced56b5e14a87be
Matt <Matthew.torres@vtsd.com>
2024-03-22 09:45:23 -0400

    2.1.3

--- 2.1.4/main.py
+++ 2.1.4/main.py
@@ -2,13 +2,25 @@
 #   A program creates a window on your screen using Tkinter.
 import tkinter as tk
 
+def test_my_button():
+    if(ent_username.get() == "username" and ent_password.get() == "pass"):
+        frame_auth.tkraise()
+    else:
+        fail_label = tk.Label(frame_login, text="Invali. Try again")
+        fail_label.pack()
+
+        frame_auth.tkraise()
+
 # main window
 root = tk.Tk()
-root.wm_geometry("400x400")
+root.wm_geometry("200x200")
 
 # create empty frame
 frame_login = tk.Frame(root)
 frame_login.grid()
+
+frame_auth = tk.Frame(root)
+frame_auth.grid()
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
@@ -25,4 +37,8 @@ lbl_username.pack()
 ent_username = tk.Entry(frame_login, bd=3)
 ent_username.pack(pady=5)
 
+btn_login = tk.Button(frame_login, text="login", command=test_my_button())
+btn_login.pack()
+
+frame_login.tkraise()
 root.mainloop()
